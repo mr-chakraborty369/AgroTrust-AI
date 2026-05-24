9b90912b20993fa033b9a9b3cde47f0bb156f528
beraaniket <beraaniket98@gmail.com>
2026-05-24 19:51:47 +0530

    fix: disable XFrameOptionsMiddleware to allow Hugging Face Space iframe embedding

--- agrotrust_django/agrotrust_django/settings.py
+++ agrotrust_django/agrotrust_django/settings.py
@@ -53,8 +53,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 ROOT_URLCONF = 'agrotrust_django.urls'
 
